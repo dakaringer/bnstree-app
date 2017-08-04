d8a2585c8b89d2e2b68c5c44ec5f3cae500ce897
dakaringer <dakaringer@gmail.com>
2017-08-04 17:55:51 -0400

    add update notification

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -7,6 +7,7 @@
 
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
+import {notification} from 'antd'
 
 const isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
@@ -56,6 +57,11 @@ function registerValidSW(swUrl) {
                             // It's the perfect time to display a "New content is
                             // available; please refresh." message in your web app.
                             console.log('New content is available; please refresh.')
+                            notification.info({
+                                message: 'New Update',
+                                description: 'New update is available. Please refresh.',
+                                placement: 'bottomRight'
+                            })
                         } else {
                             // At this point, everything has been precached.
                             // It's the perfect time to display a
