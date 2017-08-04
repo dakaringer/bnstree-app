b2e204a76eaf426bb6c395e36ee490410da83088
dakaringer <dakaringer@gmail.com>
2017-08-04 17:22:50 -0400

    use pathname to determine whether to show search

--- src/components/Character/components/CharacterHeader.js
+++ src/components/Character/components/CharacterHeader.js
@@ -1,21 +1,13 @@
 import React from 'react'
-import {connect} from 'react-redux'
 import {translate} from 'react-i18next'
+import {withRouter} from 'react-router'
 
 import icon from '../images/GameUI_HeaderIcon_160.png'
 
-import {characterSelector} from '../selectors'
-
 import CharacterSearch from './CharacterSearch'
 
-const mapStateToProps = state => {
-    return {
-        character: characterSelector(state)
-    }
-}
-
 const CharacterHeader = props => {
-    const {t, character} = props
+    const {t, location} = props
 
     return (
         <div className="character-header section-header">
@@ -26,10 +18,14 @@ const CharacterHeader = props => {
                 </span>
             </div>
             <div className="header-item">
-                {character.has('general') ? <CharacterSearch /> : null}
+                {location.pathname !== '/character' &&
+                location.pathname !== '/character/na' &&
+                location.pathname !== '/character/eu'
+                    ? <CharacterSearch />
+                    : null}
             </div>
         </div>
     )
 }
 
-export default connect(mapStateToProps)(translate('general')(CharacterHeader))
+export default withRouter(translate('general')(CharacterHeader))
