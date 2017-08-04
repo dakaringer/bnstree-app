1db83eaa708be9f453e9334ceff7e341cfe388b6
dakaringer <dakaringer@gmail.com>
2017-08-04 17:23:35 -0400

    add character search to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 import './styles/Home.scss'
 
 import ClassLinks from './components/HomeClassLinks'
+import CharacterSearch from '../Character/components/CharacterSearch'
 
 class Home extends Component {
     componentDidMount() {
@@ -13,6 +14,7 @@ class Home extends Component {
         return (
             <div className="home">
                 <ClassLinks />
+                <CharacterSearch center />
             </div>
         )
     }
