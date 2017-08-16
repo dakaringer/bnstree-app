d4985b471d1e878fc3bd18f69f6aad312ce3dc06
dakaringer <dakaringer@gmail.com>
2017-08-16 06:08:54 -0400

    add class switcher

--- src/components/Classes/components/ClassHeader.js
+++ src/components/Classes/components/ClassHeader.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {translate} from 'react-i18next'
+import {NavLink, withRouter} from 'react-router-dom'
 
+import {Icon, Popover} from 'antd'
+
+import {classes} from '../../NavBar/NavBar'
 import classImages from '../images/map_classImg'
 
 import {classSelector} from '../selectors'
@@ -12,19 +16,45 @@ const mapStateToProps = state => {
     }
 }
 
-const SkillHeader = props => {
-    const {t, classCode} = props
+const ClassHeader = props => {
+    const {t, classCode, location} = props
+
+    let currentPath = location.pathname.split('/').slice(-1)[0]
+
+    let classLinks = []
+    classes.forEach(c => {
+        classLinks.push(
+            <NavLink
+                to={`/classes/${c[1]}/${currentPath !== c[1] ? currentPath : ''}`}
+                className="class-link"
+                key={c[0]}>
+                <img alt={c[1]} src={classImages[c[0]]} />
+                <p>
+                    {t(c[0])}
+                </p>
+            </NavLink>
+        )
+    })
 
     return (
-        <div className="skill-header section-header">
-            <div className="header-title">
-                <img alt={classCode} src={classImages[classCode]} />
-                <span>
-                    {t(classCode)}
-                </span>
+        <div className="class-header section-header">
+            <div className="header-title class-selector">
+                <Popover
+                    trigger="click"
+                    placement="bottomRight"
+                    content={
+                        <div className="class-selector-popover">
+                            {classLinks}
+                        </div>
+                    }>
+                    <img alt={classCode} src={classImages[classCode]} />
+                    <div>
+                        {t(classCode)} <Icon type="down" />
+                    </div>
+                </Popover>
             </div>
         </div>
     )
 }
 
-export default connect(mapStateToProps)(translate('general')(SkillHeader))
+export default withRouter(connect(mapStateToProps)(translate('general')(ClassHeader)))
