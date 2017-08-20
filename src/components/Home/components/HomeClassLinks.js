e581e6391d0d80270274091cbf3a4512ee3de30d
dakaringer <dakaringer@gmail.com>
2017-08-20 04:55:07 -0400

    change to functional component

--- src/components/Home/components/HomeClassLinks.js
+++ src/components/Home/components/HomeClassLinks.js
@@ -1,31 +1,29 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {translate} from 'react-i18next'
 import {Link} from 'react-router-dom'
 
 import classImages from '../../Classes/images/map_classImg'
 import {classes} from '../../NavBar/NavBar'
 
-class HomeClassLinks extends Component {
-    render() {
-        const {t} = this.props
-        let links = []
-        classes.forEach(c => {
-            links.push(
-                <Link to={`/classes/${c[1]}`} className="class-link" key={c[0]}>
-                    <img alt={c[1]} src={classImages[c[0]]} />
-                    <p>
-                        {t(c[0])}
-                    </p>
-                </Link>
-            )
-        })
-
-        return (
-            <div className="home-class-links">
-                {links}
-            </div>
+const HomeClassLinks = props => {
+    const {t} = props
+    let links = []
+    classes.forEach(c => {
+        links.push(
+            <Link to={`/classes/${c[1]}`} className="class-link" key={c[0]}>
+                <img alt={c[1]} src={classImages[c[0]]} />
+                <p>
+                    {t(c[0])}
+                </p>
+            </Link>
         )
-    }
+    })
+
+    return (
+        <div className="home-class-links">
+            {links}
+        </div>
+    )
 }
 
 export default translate('general')(HomeClassLinks)
