d1eac9ed33eaef2174f122fec8c9ce5da9060287
dakaringer <dakaringer@gmail.com>
2017-10-15 14:42:23 -0400

    add local cache support for language fetch

--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,38 @@
 import i18n from 'i18next'
 import Fetch from 'i18next-fetch-backend'
+import localforage from 'localforage'
 import apollo, {q} from './apollo'
 
-function fetchData(url, options, callback) {
-    let query = q`query i18next{
-        Languages {
-            languages (${url})
-        } 
-     }`
+let query = q`query i18next(
+    $language: String!, 
+    $namespace: String!
+){
+    Languages {
+        languages (
+            language: $language,
+            namespace: $namespace
+        )
+    } 
+}`
+
+async function fetchData(variables, options, callback) {
+    variables = JSON.parse(variables)
+    let key = `${variables.language}:${variables.namespace}`
+    let data = await localforage.getItem(key)
+    if (data) {
+        callback(data, {status: '200'})
+    }
 
     apollo
-        .query({query: query})
-        .then(json => json.data.Languages)
-        .then(locale => callback(locale, {status: '200'}))
+        .query({
+            query: query,
+            variables: variables
+        })
+        .then(json => {
+            data = json.data.Languages
+            localforage.setItem(key, data)
+            callback(data, {status: '200'})
+        })
         .catch(e => callback(e, {status: '404'}))
 }
 
@@ -22,7 +42,7 @@ i18n.use(Fetch).init({
     ns: 'general',
     fallbackNS: 'general',
     defaultNS: 'general',
-    //debug: process.env.NODE_ENV !== 'production',
+    debug: process.env.NODE_ENV !== 'production',
     preload: ['en'],
     interpolation: {
         escapeValue: false // not needed for react!!
@@ -35,8 +55,8 @@ i18n.use(Fetch).init({
             method: 'get',
             credentials: 'include'
         },
-        loadPath: 'language: "{{lng}}", namespace: "{{ns}}"',
-        addPath: 'language: "{{lng}}", namespace: "{{ns}}"',
+        loadPath: '{"language": "{{lng}}", "namespace": "{{ns}}"}',
+        addPath: '{"language": "{{lng}}", "namespace": "{{ns}}"}',
         ajax: fetchData
     },
     react: {
