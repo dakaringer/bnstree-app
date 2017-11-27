bfc8dd8bf5c6782150362dba634c3d7e4ec11e8b
dakaringer <dakaringer@gmail.com>
2017-11-27 12:32:46 -0500

    switch to functional component

--- src/components/Items/components/ItemListItem.js
+++ src/components/Items/components/ItemListItem.js
@@ -16,58 +16,54 @@ const mapStateToProps = state => {
     }
 }
 
-class BadgeListItem extends React.PureComponent {
-    render() {
-        const {t, type, item, itemId} = this.props
+const BadgeListItem = props => {
+    const {t, type, item, itemId} = props
 
-        let subtitle = []
-        let content = null
-        switch (type) {
-            case 'badges':
-                content = <BadgeContent item={item} itemId={itemId} />
-                break
-            case 'soulshields':
-                if (item.get('group')) {
-                    subtitle.push(<p key="group">{t(item.get('group'))}</p>)
-                }
-                content = <SoulshieldContent item={item} itemId={itemId} />
-                break
-            default:
-                content = null
-        }
-
-        if (item.get('classCode') && item.get('classCode') !== 'ALL') {
-            subtitle.push(<p key="class">{t(item.get('classCode'))}</p>)
-        }
-
-        return (
-            <div className={`item-list-item ${type}`}>
-                <Collapse bordered={false}>
-                    <Panel
-                        header={
-                            <div className="item-header">
-                                <img
-                                    className="item-icon"
-                                    alt={item.get('name')}
-                                    src={`https://static.bnstree.com/images/${type}/${item.get(
-                                        'icon',
-                                        'blank'
-                                    )}`}
-                                />
-                                <div className="title">
-                                    <h3 className={`grade_${item.get('grade')}`}>
-                                        {item.get('name')}
-                                    </h3>
-                                    <div className="subtitle">{subtitle}</div>
-                                </div>
-                            </div>
-                        }>
-                        {content}
-                    </Panel>
-                </Collapse>
-            </div>
-        )
+    let subtitle = []
+    let content = null
+    switch (type) {
+        case 'badges':
+            content = <BadgeContent item={item} itemId={itemId} />
+            break
+        case 'soulshields':
+            if (item.get('group')) {
+                subtitle.push(<p key="group">{t(item.get('group'))}</p>)
+            }
+            content = <SoulshieldContent item={item} itemId={itemId} />
+            break
+        default:
+            content = null
     }
+
+    if (item.get('classCode') && item.get('classCode') !== 'ALL') {
+        subtitle.push(<p key="class">{t(item.get('classCode'))}</p>)
+    }
+
+    return (
+        <div className={`item-list-item ${type}`}>
+            <Collapse bordered={false}>
+                <Panel
+                    header={
+                        <div className="item-header">
+                            <img
+                                className="item-icon"
+                                alt={item.get('name')}
+                                src={`https://static.bnstree.com/images/${type}/${item.get(
+                                    'icon',
+                                    'blank'
+                                )}`}
+                            />
+                            <div className="title">
+                                <h3 className={`grade_${item.get('grade')}`}>{item.get('name')}</h3>
+                                <div className="subtitle">{subtitle}</div>
+                            </div>
+                        </div>
+                    }>
+                    {content}
+                </Panel>
+            </Collapse>
+        </div>
+    )
 }
 
 export default connect(mapStateToProps)(translate('items')(BadgeListItem))
