a41083c109e49610dcd8fb807c4887ddf398ff78
dakaringer <dakaringer@gmail.com>
2017-11-29 09:19:50 -0500

    optimize images + add new characters

--- src/components/Home/images/map_overlayImg.js
+++ src/components/Home/images/map_overlayImg.js
@@ -2,14 +2,18 @@ import left_1 from './overlay_1_left.png'
 import right_1 from './overlay_1_right.png'
 import left_2 from './overlay_2_left.png'
 import right_3 from './overlay_3_right.png'
-import right_4 from './overlay_4_right.png'
+//import right_4 from './overlay_4_right.png'
 import left_5 from './overlay_5_left.png'
 import right_5 from './overlay_5_right.png'
 import right_6 from './overlay_6_right.png'
-import center_7 from './overlay_7.png'
+import center_7 from './overlay_7_center.png'
+import left_8 from './overlay_8_left.png'
+import left_9 from './overlay_9_left.png'
+import right_9 from './overlay_9_right.png'
+import left_10 from './overlay_10_left.png'
 
-const overlayImages = {
-    set_1: {
+const overlayImages = [
+    {
         left: left_1,
         leftStyle: {
             bottom: '-20rem',
@@ -21,20 +25,20 @@ const overlayImages = {
             height: '124%'
         }
     },
-    set_2: {
+    {
         left: left_2
     },
-    set_3: {
+    {
         right: right_3,
         rightStyle: {
             bottom: '-5rem',
             height: '110%'
         }
     },
-    set_4: {
-        right: right_4
-    },
-    set_5: {
+    //    {
+    //      right: right_4
+    //},
+    {
         left: left_5,
         leftStyle: {
             left: '7rem',
@@ -46,16 +50,35 @@ const overlayImages = {
             right: '0'
         }
     },
-    set_6: {
+    {
         right: right_6,
         rightStyle: {
             right: '-10rem',
             height: '95%'
         }
     },
-    set_7: {
+    {
         center: center_7
+    },
+    {
+        left: left_8,
+        leftStyle: {
+            left: '-8rem'
+        }
+    },
+    {
+        left: left_9,
+        leftStyle: {
+            left: '2rem'
+        },
+        right: right_9,
+        rightStyle: {
+            right: '2rem'
+        }
+    },
+    {
+        left: left_10
     }
-}
+]
 
 export default overlayImages
