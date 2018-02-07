01ae90a3d7ad7ea86f2916ebe2b09e3c8caf6084
dakaringer <dakaringer@gmail.com>
2018-02-06 23:28:59 -0500

    adjust markdown toc level

--- src/components/Editor/components/Article.js
+++ src/components/Editor/components/Article.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import MarkdownIt from 'markdown-it'
 import MarkdownItMark from 'markdown-it-mark'
 import MarkdownItAnchor from 'markdown-it-anchor'
 import MarkdownItToC from 'markdown-it-table-of-contents'
-import {Map} from 'immutable'
+import { Map } from 'immutable'
 
-import {skillNamesSelectorEN} from '../../References/selectors'
+import { skillNamesSelectorEN } from '../../References/selectors'
 
 const mapStateToProps = state => {
     return {
@@ -21,12 +21,12 @@ let md = new MarkdownIt('default', {
     .use(MarkdownItMark)
     .use(MarkdownItAnchor)
     .use(MarkdownItToC, {
-        includeLevel: [3]
+        includeLevel: [2, 3]
     })
 
 class EditorPreview extends React.PureComponent {
     parseSkill(match, id) {
-        let {skillNames} = this.props
+        let { skillNames } = this.props
 
         let skill = skillNames.get(id, Map())
         return `==![skill-${id}](https://static.bnstree.com/images/skills/${skill.get(
@@ -36,7 +36,7 @@ class EditorPreview extends React.PureComponent {
     }
 
     render() {
-        let {article} = this.props
+        let { article } = this.props
 
         let renderedContent = article
             .get('content', '')
@@ -46,7 +46,7 @@ class EditorPreview extends React.PureComponent {
         return (
             <div
                 className="editor-preview article"
-                dangerouslySetInnerHTML={{__html: md.render(renderedContent)}}
+                dangerouslySetInnerHTML={{ __html: md.render(renderedContent) }}
             />
         )
     }
