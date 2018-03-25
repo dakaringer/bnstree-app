1e9b5542b4640673b5df3d4b8579d5453ee5e0c3
dakaringer <dakaringer@gmail.com>
2018-03-25 01:20:52 -0400

    refactor / keep code dry

--- src/components/Character/components/CharacterStats.js
+++ src/components/Character/components/CharacterStats.js
@@ -11,23 +11,13 @@ import CharacterPointMenu from './CharacterPointMenu'
 
 import hmPointImages from '../images/map_hmPointImg'
 
-const mapStateToProps = state => {
-    return {
-        statData: characterSelector(state).get('statData'),
-        classCode: characterSelector(state).getIn(['general', 'classCode'])
-    }
-}
-
 const attackStats = [
     ['attack_power_value', []],
     ['pc_attack_power_value', []],
     ['boss_attack_power_value', []],
     ['attack_pierce_value', ['attack_defend_pierce_rate', 'attack_parry_pierce_rate']],
     ['attack_hit_value', ['attack_hit_rate']],
-    [
-        'attack_concentrate_value',
-        ['attack_perfect_parry_damage_rate', 'attack_counter_pierce_rate']
-    ],
+    ['attack_concentrate_value', ['attack_perfect_parry_damage_rate', 'attack_counter_pierce_rate']],
     ['attack_critical_value', ['attack_critical_rate']],
     ['attack_critical_damage_value', ['attack_critical_damage_rate']],
     ['attack_stiff_duration_level', []],
@@ -44,27 +34,11 @@ const attackStats = [
 
 const defenseStats = [
     ['max_hp', []],
-    [
-        'defend_power_value',
-        [
-            'defend_physical_damage_reduce_rate',
-            'aoe_defend_power_value',
-            'aoe_defend_damage_reduce_rate'
-        ]
-    ],
-    [
-        'pc_defend_power_value',
-        ['pc_defend_power_rate', 'aoe_defend_power_value', 'aoe_defend_damage_reduce_rate']
-    ],
-    [
-        'boss_defend_power_value',
-        ['boss_defend_power_rate', 'aoe_defend_power_value', 'aoe_defend_damage_reduce_rate']
-    ],
+    ['defend_power_value', ['defend_physical_damage_reduce_rate', 'aoe_defend_power_value', 'aoe_defend_damage_reduce_rate']],
+    ['pc_defend_power_value', ['pc_defend_power_rate', 'aoe_defend_power_value', 'aoe_defend_damage_reduce_rate']],
+    ['boss_defend_power_value', ['boss_defend_power_rate', 'aoe_defend_power_value', 'aoe_defend_damage_reduce_rate']],
     ['defend_dodge_value', ['defend_dodge_rate', 'counter_damage_reduce_rate']],
-    [
-        'defend_parry_value',
-        ['defend_parry_rate', 'defend_parry_reduce_rate', 'perfect_parry_damage_reduce_rate']
-    ],
+    ['defend_parry_value', ['defend_parry_rate', 'defend_parry_reduce_rate', 'perfect_parry_damage_reduce_rate']],
     ['defend_critical_value', ['defend_critical_rate', 'defend_critical_damage_rate']],
     ['defend_stiff_duration_level', []],
     ['defend_damage_modify_diff', ['defend_damage_modify_rate']],
@@ -86,78 +60,76 @@ const classElements = {
     SH: ['attack_attribute_fire_value', 'attack_attribute_void_value']
 }
 
-const hmPointAttackExtra = [['threat', 0], ['energy', 3]]
-const hmPointDefenseExtra = [['regen', 1], ['speed', 2], ['status', 4]]
+const hmPointAttackBuffs = [['threat', 0], ['energy', 3]]
+const hmPointDefenseBuffs = [['regen', 1], ['speed', 2], ['status', 4]]
+
+const mapStateToProps = state => {
+    return {
+        statData: characterSelector(state).get('statData', Map()),
+        classCode: characterSelector(state).getIn(['general', 'classCode'])
+    }
+}
 
 const CharacterStats = props => {
     let { t, classCode, statData, type } = props
-    statData = statData || Map()
 
-    let stats = null
-    let hmPoint = 0
-    let hmPointMenu = null
-    let hmPointExtra = []
-    let extraEffects = []
+    let buffs = []
+    let statList = []
+    let point = ''
+    let mainStat = null
+    let mainStatValue = null
 
     if (type === 'attack') {
-        let a = attackStats.filter(stat => {
+        statList = attackStats.filter(stat => {
             return (
                 !stat[0].startsWith('attack_attribute') ||
                 (classElements[classCode][0] === stat[0] || classElements[classCode][1] === stat[0])
             )
         })
+        point = 'offense_point'
+        buffs = hmPointAttackBuffs
 
-        stats = <CharacterStatList stats={a} type="attack" />
-        hmPoint = statData.getIn(['point_ability', 'offense_point'], 0)
-
-        hmPointAttackExtra.forEach(e => {
-            let [t, i] = e
-            let p = statData.getIn(['point_ability', 'picks', i, 'point'], 0)
-
-            if (p !== 0) {
-                extraEffects.push(t)
-            }
-
-            hmPointExtra.push(
-                <div className={`hmPoint-item ${p === 0 ? 'disabled' : ''}`} key={t}>
-                    <img src={hmPointImages[t]} alt={`hmPoint-${t}`} />
-                    {p}P
-                </div>
-            )
-        })
-
-        hmPointMenu = <CharacterPointMenu points={hmPoint} extraEffects={extraEffects} type="attack" />
+        mainStat = 'attack_power_value'
+        mainStatValue = statData.getIn(['total_ability', 'attack_power_value'], 0)
     } else {
-        stats = <CharacterStatList stats={defenseStats} type="defend" />
-        hmPoint = statData.getIn(['point_ability', 'defense_point'], 0)
+        statList = defenseStats
+        point = 'defense_point'
+        buffs = hmPointDefenseBuffs
 
-        hmPointDefenseExtra.forEach(e => {
-            let [t, i] = e
-            let p = statData.getIn(['point_ability', 'picks', i, 'point'], 0)
-
-            if (p !== 0) {
-                extraEffects.push(t)
-            }
-
-            hmPointExtra.push(
-                <div className={`hmPoint-item ${p === 0 ? 'disabled' : ''}`} key={t}>
-                    <img src={hmPointImages[t]} alt={`hmPoint-${t}`} />
-                    {p}P
-                </div>
-            )
-        })
-
-        hmPointMenu = <CharacterPointMenu points={hmPoint} extraEffects={extraEffects} type="defense" />
+        mainStat = 'max_hp'
+        mainStatValue = statData.getIn(['total_ability', 'max_hp'], 0)
     }
+
+    let stats = null
+    let hmPointMenu = null
+    let hmPoint = statData.getIn(['point_ability', point], 0)
+    let hmPointBuffs = []
+    let extraEffects = []
+
+    stats = <CharacterStatList stats={statList} type="defend" />
+    buffs.forEach(e => {
+        let [t, i] = e
+        let p = statData.getIn(['point_ability', 'picks', i, 'point'], 0)
+
+        if (p !== 0) {
+            extraEffects.push(t)
+        }
+
+        hmPointBuffs.push(
+            <div className={`hmPoint-item ${p === 0 ? 'disabled' : ''}`} key={t}>
+                <img src={hmPointImages[t]} alt={`hmPoint-${t}`} />
+                {p}P
+            </div>
+        )
+    })
+    hmPointMenu = <CharacterPointMenu points={hmPoint} extraEffects={extraEffects} type={type} />
 
     return (
         <div className={`character-stats ${type}`}>
             <div className="mainStat">
-                <h4>{type === 'attack' ? t('attack_power_value') : t('max_hp')}</h4>
+                <h4>{t(mainStat)}</h4>
                 <h3 className="mainValue">
-                    {type === 'attack'
-                        ? statData.getIn(['total_ability', 'attack_power_value'], 0)
-                        : statData.getIn(['total_ability', 'max_hp'], 0)}
+                    {mainStatValue}
                 </h3>
                 <Tooltip
                     placement="bottom"
@@ -166,16 +138,18 @@ const CharacterStats = props => {
                     overlayClassName="hmPointMenu">
                     <div className="hmPoint">
                         <div className="hmPoint-main hmPoint-item">
-                            <img src={hmPointImages[type]} alt='hmPoint-attack' />
+                            <img src={hmPointImages[type]} alt={`hmPoint-${type}`} />
                             {hmPoint}P
                         </div>
                         <div className="hmPoint-extra">
-                            {hmPointExtra}
+                            {hmPointBuffs}
                         </div>
                     </div>
                 </Tooltip>
             </div>
-            <div className="subStatsContainer">{stats}</div>
+            <div className="subStatsContainer">
+                {stats}
+            </div>
         </div>
     )
 }
