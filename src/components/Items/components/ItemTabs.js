bb7e15501cd4023ec06571af3e59f5202166421b
dakaringer <dakaringer@gmail.com>
2018-04-01 10:58:57 -0400

    add variable height list

--- src/components/Items/components/ItemTabs.js
+++ src/components/Items/components/ItemTabs.js
@@ -23,18 +23,34 @@ const BadgeList = props => {
     let tabs = []
     data.forEach((group, key) => {
         let items = []
-        group.forEach((item, id) => items.push(<ItemListItem item={item} itemId={id} key={id} />))
+        let itemColumn1 = []
+        let itemColumn2 = []
+        let i = 0
+        group.forEach((item, id) => {
+            let component = <ItemListItem item={item} itemId={id} key={id} />
+            items.push(component)
+            if (i % 2 === 0) {
+                itemColumn1.push(component)
+            } else {
+                itemColumn2.push(component)
+            }
+            i++
+        })
         tabs.push(
             <TabPane tab={t(key)} key={key}>
                 <Fade>
-                    <div className="item-list">{items}</div>
+                    <div className="item-list-container">
+                        <div className="item-list item-list-single">{items}</div>
+                        <div className="item-list item-list-column">{itemColumn1}</div>
+                        <div className="item-list item-list-column">{itemColumn2}</div>
+                    </div>
                 </Fade>
             </TabPane>
         )
     })
 
     return (
-        <div className="item-list-container">
+        <div className="item-container">
             <ItemSubMenu />
             <Tabs animated>{tabs}</Tabs>
         </div>
