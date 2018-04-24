2eaa7efc4662ec57c91d106ffaa44aac52d3d3e6
Robin Hong <dakaringer@gmail.com>
2018-04-23 20:22:18 -0400

    disable service worker

--- server/characterRenderer.js
+++ server/characterRenderer.js
@@ -1,97 +1,94 @@
-const path = require("path")
-const fs = require("fs")
+const path = require('path')
+const fs = require('fs')
 
 const gql = require('graphql-tag')
 const gqlClient = require('./apollo')
 
 const characterQuery = gql`
-    query($name: String!, $region: String!) {
-        Character(name: $name, region: $region) {
-            general 
-            statData: stats
-        }
-    }
+	query($name: String!, $region: String!) {
+		Character(name: $name, region: $region) {
+			general
+			statData: stats
+		}
+	}
 `
 
 const classElements = {
-    BM: ['attack_attribute_fire_value', 'attack_attribute_lightning_value'],
-    KF: ['attack_attribute_fire_value', 'attack_attribute_wind_value'],
-    DE: ['attack_attribute_earth_value', 'attack_attribute_void_value'],
-    FM: ['attack_attribute_fire_value', 'attack_attribute_ice_value'],
-    AS: ['attack_attribute_lightning_value', 'attack_attribute_void_value'],
-    SU: ['attack_attribute_wind_value', 'attack_attribute_earth_value'],
-    BD: ['attack_attribute_wind_value', 'attack_attribute_lightning_value'],
-    WL: ['attack_attribute_ice_value', 'attack_attribute_void_value'],
-    SF: ['attack_attribute_ice_value', 'attack_attribute_earth_value'],
-    SH: ['attack_attribute_fire_value', 'attack_attribute_void_value']
+	BM: ['attack_attribute_fire_value', 'attack_attribute_lightning_value'],
+	KF: ['attack_attribute_fire_value', 'attack_attribute_wind_value'],
+	DE: ['attack_attribute_earth_value', 'attack_attribute_void_value'],
+	FM: ['attack_attribute_fire_value', 'attack_attribute_ice_value'],
+	AS: ['attack_attribute_lightning_value', 'attack_attribute_void_value'],
+	SU: ['attack_attribute_wind_value', 'attack_attribute_earth_value'],
+	BD: ['attack_attribute_wind_value', 'attack_attribute_lightning_value'],
+	WL: ['attack_attribute_ice_value', 'attack_attribute_void_value'],
+	SF: ['attack_attribute_ice_value', 'attack_attribute_earth_value'],
+	SH: ['attack_attribute_fire_value', 'attack_attribute_void_value']
 }
 
 const elements = {
-    attack_attribute_fire_value: 'Flame Damage',
-    attack_attribute_ice_value: 'Frost Damage',
-    attack_attribute_wind_value: 'Wind Damage',
-    attack_attribute_earth_value: 'Earth Damage',
-    attack_attribute_lightning_value: 'Lightning Damage',
-    attack_attribute_void_value: 'Shadow Damage'
+	attack_attribute_fire_value: 'Flame Damage',
+	attack_attribute_ice_value: 'Frost Damage',
+	attack_attribute_wind_value: 'Wind Damage',
+	attack_attribute_earth_value: 'Earth Damage',
+	attack_attribute_lightning_value: 'Lightning Damage',
+	attack_attribute_void_value: 'Shadow Damage'
 }
 
 export default async (req, res, next) => {
-    let name = req.params.name
-    let region = req.params.region
+	let name = req.params.name
+	let region = req.params.region
 
-    let buildPath = process.env.NODE_ENV === 'production' ? 'build_final' : 'build'
-    const filePath = path.resolve(__dirname, '..', buildPath, 'index.html')
+	let buildPath = process.env.NODE_ENV === 'production' ? 'build_final' : 'build'
+	const filePath = path.resolve(__dirname, '..', buildPath, 'index.html')
 
-    let metadata = await new Promise((resolve, reject) => {
-        gqlClient
-            .query({
-                query: characterQuery,
-                variables: {
-                    name: name,
-                    region: region
-                }
-            }).then(json => {
-                let character = json.data.Character
-                if (!character.notFound) {
-                    let desc = ''
-                    desc += `AP: ${character.statData.total_ability.attack_power_value} | `
-                    desc += `CHR: ${character.statData.total_ability.attack_critical_rate}% | `
-                    desc += `CHD: ${character.statData.total_ability.attack_critical_damage_rate}% | `
-                    desc += `Accuracy: ${character.statData.total_ability.attack_hit_rate}% | `
+	let metadata = await new Promise((resolve, reject) => {
+		gqlClient
+			.query({
+				query: characterQuery,
+				variables: {
+					name: name,
+					region: region
+				}
+			})
+			.then(json => {
+				let character = json.data.Character
+				if (!character.notFound) {
+					let desc = ''
+					desc += `AP: ${character.statData.total_ability.attack_power_value} | `
+					desc += `CHR: ${character.statData.total_ability.attack_critical_rate}% | `
+					desc += `CHD: ${character.statData.total_ability.attack_critical_damage_rate}% | `
+					desc += `Accuracy: ${character.statData.total_ability.attack_hit_rate}% | `
 
-                    classElements[character.general.classCode].forEach(e => {
-                        let rate = character.statData.total_ability[e.substr(0, e.length - 5) + 'rate']
-                        desc += `${elements[e]} ${rate}% | `
-                    })
-                    desc = desc.slice(0, -3)
+					classElements[character.general.classCode].forEach(e => {
+						let rate = character.statData.total_ability[e.substr(0, e.length - 5) + 'rate']
+						desc += `${elements[e]} ${rate}% | `
+					})
+					desc = desc.slice(0, -3)
 
-                    resolve(
-                        `
+					resolve(
+						`
                         <meta property="og:site_name" content="BnSTree">
-                        <meta property="og:title" content="${character.general.name} | Level ${character.general.level[0]}${character.general.level[1] ? ` • HM ${character.general.level[1]}` : ''} ${character.general.className} | ${character.general.region.toUpperCase()}-${character.general.server}" />
+                        <meta property="og:title" content="${character.general.name} | Level ${character.general.level[0]}${
+							character.general.level[1] ? ` • HM ${character.general.level[1]}` : ''
+						} ${character.general.className} | ${character.general.region.toUpperCase()}-${character.general.server}" />
                         <meta property="og:image" content="https://static.bnstree.com/images/class/${character.general.classCode}.png" />
                         <meta property="og:description" content="${desc}" />
                         `
-                    )
-                }
-                else {
-                    resolve('<meta property="og:site_name" content="BnSTree">')
-                }
-            })
-    })
+					)
+				} else {
+					resolve('<meta property="og:site_name" content="BnSTree">')
+				}
+			})
+	})
 
-    fs.readFile(filePath, 'utf8', (err, htmlData) => {
-        if (err) {
-            console.error('err', err);
-            return res.status(404).end()
-        }
-        //const html = ReactDOMServer.renderToString(<App />)
+	fs.readFile(filePath, 'utf8', (err, htmlData) => {
+		if (err) {
+			console.error('err', err)
+			return res.status(404).end()
+		}
+		//const html = ReactDOMServer.renderToString(<App />)
 
-        return res.send(
-            htmlData.replace(
-                '<meta property="og:site_name" content="BnSTree">',
-                metadata
-            )
-        )
-    })
+		return res.send(htmlData.replace('<meta property="og:site_name" content="BnSTree">', metadata))
+	})
 }
--- src/index.js
+++ src/index.js
@@ -2,62 +2,62 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import {createStore, applyMiddleware} from 'redux'
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 //import localForage from 'localforage'
 //import { persistStore } from 'redux-persist'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import {Map} from 'immutable'
-import {BrowserRouter, Route} from 'react-router-dom'
+import { Map } from 'immutable'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import i18n from './i18n'
-import {I18nextProvider} from 'react-i18next'
+import { I18nextProvider } from 'react-i18next'
 
 import rootReducer from './rootReducer'
-import {initialize} from './actions'
+import { initialize } from './actions'
 
 import './styles/index.scss'
 
 import ReactGA from 'react-ga'
 ReactGA.initialize('UA-61749626-5')
 const withTracker = WrappedComponent => {
-    const trackPage = page => {
-        ReactGA.set({page})
-        ReactGA.pageview(page)
-    }
+	const trackPage = page => {
+		ReactGA.set({ page })
+		ReactGA.pageview(page)
+	}
 
-    const HOC = props => {
-        const page = props.location.pathname
-        trackPage(page)
+	const HOC = props => {
+		const page = props.location.pathname
+		trackPage(page)
 
-        return <WrappedComponent {...props} />
-    }
+		return <WrappedComponent {...props} />
+	}
 
-    return HOC
+	return HOC
 }
 
 const composeEnhancers = composeWithDevTools({})
 const store = createStore(rootReducer, Map(), composeEnhancers(applyMiddleware(thunk)))
 
 class Root extends React.PureComponent {
-    componentDidMount() {
-        let language = navigator.language.substring(0, 2)
-        store.dispatch(initialize(language))
-    }
+	componentDidMount() {
+		let language = navigator.language.substring(0, 2)
+		store.dispatch(initialize(language))
+	}
 
-    render() {
-        return (
-            <Provider store={store}>
-                <I18nextProvider i18n={i18n}>
-                    <BrowserRouter>
-                        <Route component={withTracker(App)} />
-                    </BrowserRouter>
-                </I18nextProvider>
-            </Provider>
-        )
-    }
+	render() {
+		return (
+			<Provider store={store}>
+				<I18nextProvider i18n={i18n}>
+					<BrowserRouter>
+						<Route component={withTracker(App)} />
+					</BrowserRouter>
+				</I18nextProvider>
+			</Provider>
+		)
+	}
 }
 
 ReactDOM.render(<Root />, document.getElementById('root'))
-registerServiceWorker()
+//registerServiceWorker()
