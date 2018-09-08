43e91a5d284f32c738024b0e32aab568bc848989
Robin Hong <dakaringer@gmail.com>
2018-09-08 01:33:06 -0400

    Add placeholder front page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,91 @@
 import * as React from 'react'
-import { Typography } from '@material-ui/core'
+import { Typography, Paper, FormControlLabel, Checkbox } from '@material-ui/core'
+
+import PageContainer from '@src/components/PageContainer'
 
 const Home = () => {
 	return (
-		<div>
-			<Typography variant="display1">Display1</Typography>
-			<Typography variant="display2">Display2</Typography>
-			<Typography variant="display3">Display3</Typography>
-			<Typography variant="display4">Display4</Typography>
-			<Typography variant="headline">Headline</Typography>
-			<Typography variant="title">Title</Typography>
-			<Typography variant="subheading">Subheading</Typography>
-			<Typography variant="body1">Body1</Typography>
-			<Typography variant="body2">Body2</Typography>
-			<Typography variant="caption">Caption</Typography>
-			<Typography variant="button">Button</Typography>
-		</div>
+		<PageContainer>
+			<Paper style={{ padding: '2rem' }}>
+				<Typography variant="title" color="primary">
+					Welcome to BnSTree Next
+				</Typography>
+				<Typography>This site is currently under construction.</Typography>
+				<div style={{ marginTop: '2rem' }}>
+					<Typography>Current Progress</Typography>
+					<div style={{ marginTop: '1rem' }}>
+						<Typography variant="body2">Home</Typography>
+						<FormControlLabel control={<Checkbox checked={false} />} label="Layout" />
+					</div>
+					<div style={{ marginTop: '1rem' }}>
+						<Typography variant="body2">News</Typography>
+						<FormControlLabel control={<Checkbox checked={false} />} label="Basic Functionality" />
+						<FormControlLabel control={<Checkbox checked={false} />} label="Skill/Item Data Integration" />
+						<FormControlLabel control={<Checkbox checked={false} />} label="Editor" />
+					</div>
+					<div style={{ marginTop: '1rem' }}>
+						<Typography variant="body2">Character Search</Typography>
+						<FormControlLabel
+							control={<Checkbox checked={true} color="primary" />}
+							label="Basic Functionality"
+						/>
+						<FormControlLabel control={<Checkbox checked={true} color="primary" />} label="Stats" />
+						<FormControlLabel control={<Checkbox checked={true} color="primary" />} label="Items" />
+						<FormControlLabel control={<Checkbox checked={true} color="primary" />} label="Skills" />
+						<FormControlLabel control={<Checkbox checked={false} />} label="New KR HM Point System" />
+						<FormControlLabel control={<Checkbox checked={false} />} label="KR Gems" />
+					</div>
+					<div style={{ marginTop: '1rem' }}>
+						<Typography variant="body2">Skills</Typography>
+						<FormControlLabel
+							control={<Checkbox checked={true} color="primary" />}
+							label="Basic Functionality"
+						/>
+						<FormControlLabel
+							control={<Checkbox checked={true} color="primary" />}
+							label="Skill Data (latest KR)"
+						/>
+						<FormControlLabel control={<Checkbox checked={true} color="primary" />} label="Search" />
+						<FormControlLabel control={<Checkbox checked={false} />} label="Grid Mode" />
+						<FormControlLabel control={<Checkbox checked={false} />} label="Sorting" />
+						<FormControlLabel control={<Checkbox checked={false} />} label="Share/Import" />
+						<FormControlLabel
+							control={<Checkbox checked={false} />}
+							label="Entirely New System for 9.19 Changes"
+						/>
+					</div>
+					<div style={{ marginTop: '1rem' }}>
+						<Typography variant="body2">Items</Typography>
+						<FormControlLabel
+							control={<Checkbox checked={true} color="primary" />}
+							label="Basic Functionality"
+						/>
+						<FormControlLabel
+							control={<Checkbox indeterminate checked={true} />}
+							label="Soul Badge Data (latest KR) - WA only"
+						/>
+						<FormControlLabel
+							control={<Checkbox indeterminate checked={true} />}
+							label="Mystic Badge Data (latest KR) - WA only"
+						/>
+						<FormControlLabel control={<Checkbox checked={false} />} label="Soul Shield Data (latest KR)" />
+						<FormControlLabel control={<Checkbox checked={false} />} label="Other items" />
+					</div>
+					<div style={{ marginTop: '1rem' }}>
+						<Typography variant="body2">Market</Typography>
+						<FormControlLabel control={<Checkbox checked={false} />} label="Basic Functionality" />
+					</div>
+					<div style={{ marginTop: '1rem' }}>
+						<Typography variant="body2">Twitch</Typography>
+						<FormControlLabel control={<Checkbox checked={false} />} label="Basic Functionality" />
+					</div>
+					<div style={{ marginTop: '1rem' }}>
+						<Typography variant="body2">Misc.</Typography>
+						<FormControlLabel control={<Checkbox checked={false} />} label="Other Languages" />
+					</div>
+				</div>
+			</Paper>
+		</PageContainer>
 	)
 }
 
