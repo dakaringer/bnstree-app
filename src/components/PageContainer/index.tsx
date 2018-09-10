d1adc3a458feaffc0d8f4ddbbbeafaf402826bed
Robin Hong <dakaringer@gmail.com>
2018-09-10 03:06:53 -0400

    Transition loading state

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import * as classNames from 'classnames'
 import { Paper, Slide } from '@material-ui/core'
 import LoadingLyn from '@src/components/LoadingLyn'
+import FadeContainer from '@src/components/FadeContainer'
 
 import * as style from './styles/index.css'
 
@@ -14,19 +15,21 @@ interface Props {
 const Container: React.SFC<Props> = props => {
 	const { isLoading, children, topNav, className } = props
 
-	if (isLoading) {
-		return <LoadingLyn />
-	}
-
 	return (
-		<div className={classNames(style.container, className)}>
-			<Slide direction="down" in={Boolean(topNav)} unmountOnExit timeout={500}>
-				<Paper square className={style.topNav}>
-					{topNav}
-				</Paper>
-			</Slide>
-			<div className={style.pageContent}>{children}</div>
-		</div>
+		<FadeContainer currentKey={isLoading ? 'loading' : 'loaded'} className={classNames(style.container, className)}>
+			{isLoading ? (
+				<LoadingLyn />
+			) : (
+				<>
+					<Slide direction="down" in={Boolean(topNav)} unmountOnExit timeout={500}>
+						<Paper square className={style.topNav}>
+							{topNav}
+						</Paper>
+					</Slide>
+					<div className={style.pageContent}>{children}</div>
+				</>
+			)}
+		</FadeContainer>
 	)
 }
 
