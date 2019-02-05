bcb4d810cf2be107ac7947e7a3a7b0ef04615a62
Robin Hong <dakaringer@gmail.com>
2019-02-04 22:21:34 -0500

    Fix mobile

--- src/components/Navigation/style.ts
+++ src/components/Navigation/style.ts
@@ -1,11 +1,7 @@
-import styled, { media } from '@style/styled-components'
+import styled from '@style/styled-components'
 
 export const NavigationContainer = styled.div`
 	min-height: 100vh;
 	height: 100vh;
 	display: flex;
-
-	${media.xs`
-		height: auto;
-	`}
 `
