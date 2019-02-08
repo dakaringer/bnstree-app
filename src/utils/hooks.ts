5eb871a98cbbd26a090bf2afdba4f623099ca64d
Robin Hong <dakaringer@gmail.com>
2019-02-07 21:08:23 -0500

    Fix minor oops

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,11 +1,11 @@
-import { useEffect, useState, useRef, useRender as useReactCallback } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 
 export const useRender = <T extends (...args: any[]) => any>(callback: T): T => {
 	const ref = useRef<T>((() => null) as T)
 
 	ref.current = callback
 
-	return useReactCallback(((...args) => ref.current(...args)) as T, [])
+	return useCallback(((...args) => ref.current(...args)) as T, [])
 }
 
 export const useDebounce = (value: any, delay: number) => {
